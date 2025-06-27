fff171968223b8efe6355edd77b472d2f7dbf50e
Carlos A. <carlosvsk@gmail.com>
2025-06-27 11:44:45 -0300

    Commit generated by Flaxboll.

--- src/index.tsx
+++ src/index.tsx
@@ -654,15 +654,7 @@ console.log({item});
         (...args: any) => <Elements.Screen3 pass={{
           pathScreen:"home",
 
-          styles:[
-              {
-                backgroundColor: '#101',
-                justifyContent: 'center',
-                alignItems: 'center',
-                height: '100%',
-                width: '100%',
-              }
-              ],
+          styles:[`{ backgroundColor: "green" }`],
 
           screenElements:[() => {
                   const textStyle = {
