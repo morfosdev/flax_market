29c0a14194201a65c156df3a2a96499c4161f0ed
Leandro <leandrowebmaster@gmail.com>
2025-06-30 16:20:46 -0300

    Commit generated by Flaxboll.

--- src/index.tsx
+++ src/index.tsx
@@ -2227,14 +2227,9 @@ flexDirection: 'row',
           (...args:any) => <Elements.DynView pass={{
             elementsProperties:['{}'],
 
-            styles:[
-              {
-                backgroundColor: 'white',
-                justifyContent: 'center',
-                minHeight: 22,
-                width: "100%",
-              }
-              ],
+            styles:[`{
+flex: 1,
+}`],
 
             functions:[()=>{}],            childrenItems:[() =><></>],
 
