b1da10df7f457765c45c44c7657449fc547d5bbe
Carlos A. <carlosvsk@gmail.com>
2025-07-04 12:40:40 -0300

    Commit generated by Flaxboll.

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,25 @@
 
-  import { defineConfig } from 'vite';
-  import react from '@vitejs/plugin-react';
-  import path from 'path';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
-  export default defineConfig({
-    plugins: [react()],
-    resolve: {
-      alias: {
-        'react-native': 'react-native-web',
-        'react-native-svg': 'react-native-svg-web'
-      },
-      extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      'react-native': 'react-native-web',
+      'react-native-svg': 'react-native-svg-web'
     },
-    server: {
-      hmr: true,
-     allowedHosts: ['5173-morfosdev-flaxmarket-x08mg600rsr.ws-us120.gitpod.io'],
-      watch: {
-        usePolling: true,
-      },
+    extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
+  },
+  server: {
+    hmr: true,
+    allowedHosts: ['5173-morfosdev-flaxmarket-x08mg600rsr.ws-us120.gitpod.io'],
+    watch: {
+      usePolling: true,
     },
-  });
+  },
+  build: {
+    outDir: 'web-build', // <- nova pasta de saída
+  },
+});
