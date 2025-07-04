13972a68066cf03e950138ad8f4617f4c8586056
Carlos A. <carlosvsk@gmail.com>
2025-07-04 17:17:37 -0300

    Commit generated by Flaxboll.

--- index.tsx
+++ index.tsx
@@ -6,6 +6,13 @@ import * as RN from 'react-native';
 // ---------- import Internals
 import { Router } from './src';
 
+// ---------- inject Google Fonts
+const fontLink = document.createElement('link');
+fontLink.rel = 'stylesheet';
+fontLink.href = 'https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap';
+document.head.appendChild(fontLink);
+
+// ---------- App Component
 export default function App() {
   return <Router />;
 }
@@ -15,4 +22,3 @@ RN.AppRegistry.registerComponent('App', () => App);
 RN.AppRegistry.runApplication('App', {
   rootTag: document.getElementById('root'),
 });
-
