200743bfc1d8485274c21a06e42a1d009cc25a9f
Carlos A. <carlosvsk@gmail.com>
2025-07-04 17:34:43 -0300

    Commit generated by Flaxboll.

--- src/index.tsx
+++ src/index.tsx
@@ -66,7 +66,7 @@
           ],
 
           arrStyles: [
-            `{ fontSize: 40, fontWeight: 600, fontFamily: "Inter", fontStyle: "italic" }`
+            `{ fontSize: 40, fontWeight: 600, fontFamily: "Inter", fontStyle: 'italic' }`
           ],
 
           children: [
