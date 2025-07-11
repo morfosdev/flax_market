290642dd4d022c68215cbfd4e3dccfa6eedb017a
Leandro <leandrowebmaster@gmail.com>
2025-07-11 12:26:52 -0300

    Commit generated by Flaxboll.

--- src/index.tsx
+++ src/index.tsx
@@ -2192,10 +2192,9 @@ alignItems: 'center',
     return;
   }
 
-  console.log("Login OK");
+  console.log(tools);
 
-// Redireciona para a tela admDash
-  tools.navigateTo("admDash");
+
 }]
  , trigger: 'on press'
 }})],            childrenItems:[(...args:any) => <Elements.Text pass={{
