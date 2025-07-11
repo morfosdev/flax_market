b9a6b8ea7211f9078eef3a3a3e00d14beac4f8a7
Renan <72417996+Rehito0@users.noreply.github.com>
2025-07-11 11:45:10 -0300

    Commit generated by Flaxboll.

--- src/index.tsx
+++ src/index.tsx
@@ -140,15 +140,7 @@
         (...args: any) => <Elements.Screen3 pass={{
           pathScreen:"dsystem",
 
-          styles:[
-              {
-                backgroundColor: '#101',
-                justifyContent: 'center',
-                alignItems: 'center',
-                height: '100%',
-                width: '100%',
-              }
-              ],
+          styles:[``],
 
           screenElements:[
         
