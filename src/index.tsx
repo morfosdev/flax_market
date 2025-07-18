5c7d778538c94019f18c22ee2fcc08fac16ead5b
Renan <72417996+Rehito0@users.noreply.github.com>
2025-07-18 10:25:17 -0300

    Commit generated by Flaxboll.

--- src/index.tsx
+++ src/index.tsx
@@ -699,14 +699,9 @@ fontSize: 14,
           (...args:any) => <Elements.DynView pass={{
             elementsProperties:['{}'],
 
-            styles:[
-              {
-                backgroundColor: 'white',
-                justifyContent: 'center',
-                minHeight: 22,
-                width: "100%",
-              }
-              ],
+            styles:[`{
+  flexDirection: 'row',
+}`],
 
             functions:[()=>{}],            childrenItems:[
         (...args:any) => <Elements.Text pass={{
