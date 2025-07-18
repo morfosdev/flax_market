51c011e4245d2b76bd7d211d299895e680673c75
Leandro <leandrowebmaster@gmail.com>
2025-07-18 11:36:15 -0300

    Commit generated by Flaxboll.

--- src/index.tsx
+++ src/index.tsx
@@ -7451,56 +7451,7 @@ marginVertical: 5,
 
             styles:[`{ flex: 1, }`],
 
-            functions:[()=>{}],            childrenItems:[
-        (...args:any) => <Elements.Text pass={{
-          arrProps: [
-            '{}'
-          ],
-
-          arrStyles: [
-            { color: 'black', fontSize: 12, }
-          ],
-
-          children: [
-            `Label`
-          ],
-
-          args,
-
-        }}/>, 
-
-          (...args:any) => <Elements.DynView pass={{
-            elementsProperties:['{}'],
-
-            styles:[`{
-width: '200px',
-justifyContent: 'center',
-borderRadius: 5,
-borderWidth: 1,
-borderColor: '#eee',
-padding: 5,
-marginVertical: 5,
-}`],
-
-            functions:[()=>{}],            childrenItems:[(...args:any) => <Elements.IptTxtEdit pass={{
-          propsArray: [{}],
-
-          stylesArray: [{
-                color: 'black',
-                fontSize: 14,
-                // fontSize: '20px',<= #ATTENTION: Native ERROR! No string!
-              }],
-
-          path: [" "],
-
-          funcsArray: [() => {}],
-
-          args,
-        }}/>],
-
-            args,
-          }}/>
-        ],
+            functions:[()=>{}],            childrenItems:[() =><></>],
 
             args,
           }}/>
