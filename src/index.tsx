6422bc92a04e4ceef4fdcbb2c8c4f494c51125a5
Leandro <leandrowebmaster@gmail.com>
2025-07-18 12:48:28 -0300

    Commit generated by Flaxboll.

--- src/index.tsx
+++ src/index.tsx
@@ -8544,7 +8544,8 @@ marginVertical: 5,
 
             args,
           }}/>
-        , (...args:any) => <Elements.Text pass={{
+        , 
+        (...args:any) => <Elements.Text pass={{
           arrProps: [
             '{}'
           ],
@@ -8559,7 +8560,44 @@ marginVertical: 5,
 
           args,
 
+        }}/>, 
+
+          (...args:any) => <Elements.DynView pass={{
+            elementsProperties:['{}'],
+
+            styles:[`{
+width: '200px',
+justifyContent: 'center',
+borderRadius: 5,
+borderWidth: 1,
+borderColor: '#eee',
+padding: 5,
+marginVertical: 5,
+}`],
+
+            functions:[()=>{}],            childrenItems:[(...args:any) => <Elements.IptTxtEdit pass={{
+          propsArray: [{}],
+
+          stylesArray: [{
+                color: 'black',
+                fontSize: 14,
+                // fontSize: '20px',<= #ATTENTION: Native ERROR! No string!
+              }],
+
+          path: [`sc.admAddProducts.iptsChanges.decription`],
+
+          funcsArray: [async (...args) =>
+        functions.setVar({ args, pass:{
+          keyPath: [`sc.admAddProducts.iptsChanges.decription`],
+          value: [`$arg_callback`]
+        }})],
+
+          args,
         }}/>],
+
+            args,
+          }}/>
+        ],
 
             args,
           }}/>
