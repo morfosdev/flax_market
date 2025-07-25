1ce6e2249c318f164fc18aafe6eea3b7ec0e4782
Leandro <leandrowebmaster@gmail.com>
2025-07-25 15:55:22 -0300

    Commit generated by Flaxboll.

--- src/index.tsx
+++ src/index.tsx
@@ -14778,24 +14778,37 @@ width: '100%',
 height: '100%',
 backgroundColor: '#fff', 
 padding: 20,
+flexDirection: 'row',
  }`],
 
-            functions:[()=>{}],            childrenItems:[(...args:any) => <Elements.Text pass={{
-          arrProps: [
-            '{}'
-          ],
+            functions:[()=>{}],            childrenItems:[
 
-          arrStyles: [
-            { color: 'black', fontSize: 12, }
-          ],
+          (...args:any) => <Elements.DynView pass={{
+            elementsProperties:['{}'],
 
-          children: [
-            `Teste`
-          ],
+            styles:[`{
+width: '100%',
+alignItems: 'center',
+}`],
 
-          args,
+            functions:[()=>{}],            childrenItems:[
 
-        }}/>],
+    (...args:any) => <Elements.ImageBox pass={{
+      elementsProperties:[{}],
+
+      styles:[{
+width: '116px',
+height: '40px',
+}],
+
+      URIvariablePath:[`https://firebasestorage.googleapis.com/v0/b/devs-tests-95208.appspot.com/o/images%2FAdmin.png?alt=media&token=94ebd672-5bdd-4e25-81b7-a1b36e29e6e0`],
+
+      args,
+    }}/>],
+
+            args,
+          }}/>
+        ],
 
             args,
           }}/>
