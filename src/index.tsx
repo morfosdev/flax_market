1aca84ff5271fced67e148c4e843dd5f62e9c809
Leandro <leandrowebmaster@gmail.com>
2025-08-08 09:32:53 -0300

    Commit generated by Flaxboll.

--- src/index.tsx
+++ src/index.tsx
@@ -6722,24 +6722,7 @@ height: '100%',
 flexDirection: 'row',
  }`],
 
-            functions:[async (...args) =>
- functions.funcGroup({ args, pass:{
- arrFunctions: [
-() => {
-  const list = tools.getCtData("sc.a2.list");
-  tools.functions.setVar({
-    args: "",
-    pass: {
-      keyPath: ["sc.a2.originalList"],
-      value: [list]
-    }
-  });
-}, () => {
-  const list = tools.getCtData("sc.a2.list");
-  console.log("List is:", list);
-}]
- , trigger: 'on init'
-}})],            childrenItems:[
+            functions:[()=>{}],            childrenItems:[
         
 
           (...args:any) => <Elements.DynView pass={{
@@ -7427,7 +7410,24 @@ paddingHorizontal: 20,
 borderRadius: 10,
 }`],
 
-            functions:[()=>{}],            childrenItems:[
+            functions:[async (...args) =>
+ functions.funcGroup({ args, pass:{
+ arrFunctions: [
+() => {
+  const list = tools.getCtData("sc.a2.list");
+  tools.functions.setVar({
+    args: "",
+    pass: {
+      keyPath: ["sc.a2.originalList"],
+      value: [list]
+    }
+  });
+}, () => {
+  const list = tools.getCtData("sc.a2.list");
+  console.log("List is:", list);
+}]
+ , trigger: 'on init'
+}})],            childrenItems:[
         
 
           (...args:any) => <Elements.DynView pass={{
