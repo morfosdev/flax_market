cc9351f4a83a5c8494f62476f0c639b5913b9613
Renan <72417996+Rehito0@users.noreply.github.com>
2025-08-18 11:28:52 -0300

    Commit generated by Flaxboll.

--- src/index.tsx
+++ src/index.tsx
@@ -9572,6 +9572,20 @@ paddingHorizontal: 15,
             args,
           }}/>
         , 
+        
+
+          (...args:any) => <Elements.DynView pass={{
+            elementsProperties:['{}'],
+
+            styles:[`{
+  height: '15px',
+}`],
+
+            functions:[()=>{}],            childrenItems:[() =><></>],
+
+            args,
+          }}/>
+        , 
         (...args:any) => <Elements.Text pass={{
           arrProps: [
             '{}'
