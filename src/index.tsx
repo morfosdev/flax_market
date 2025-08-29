2b6a9bffe37bb5db9a0021f455ba5fab92c304d9
Renan <72417996+Rehito0@users.noreply.github.com>
2025-08-29 09:47:36 -0300

    Commit generated by Flaxboll.

--- src/index.tsx
+++ src/index.tsx
@@ -17448,7 +17448,7 @@ async (...args) =>
         }}), 
         (...args) => {
           // ---------- get Function from A_Project Scope
-          return tools.goTo("a2AdmProducts");
+          return tools.goTo("a5AdmOrders");
         }
         ]
  , trigger: 'on press'
