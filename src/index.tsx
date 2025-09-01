ba9f91141562af8147d57d023cf9f5ff64b7f5b2
Leandro <leandrowebmaster@gmail.com>
2025-09-01 15:45:30 -0300

    Commit generated by Flaxboll.

--- src/index.tsx
+++ src/index.tsx
@@ -7909,11 +7909,17 @@ async (...args) =>
           keyPath: [`sc.a2.list`],
           value: [`$arg_callback`]
         }})],
-        }}), async (...args) =>
+        }}), 
+async (...args) =>
         functions.setVar({ args, pass:{
           keyPath: [`sc.a2.selectedLabel`],
           value: [``]
-        }})]
+        }}), 
+        (...args) => {
+          // ---------- get Function from A_Project Scope
+          return tools.goTo("a2AdmProducts");
+        }
+        ]
  , trigger: 'on press'
 }})],            childrenItems:[(...args:any) => <Elements.SvgView1 pass={{
       componentSvg: (Svg:any, SvgObj:any) => {
