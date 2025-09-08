b7a0bcb891d12198f25443affc530ce7db200202
Leandro <leandrowebmaster@gmail.com>
2025-09-08 15:04:43 -0300

    Commit generated by Flaxboll.

--- src/index.tsx
+++ src/index.tsx
@@ -10531,7 +10531,6 @@ flexDirection: 'row',
             args,
           }}/>
         , 
-        
 
  (...args:any) => <Elements.Custom pass={{
   arrItems: [() => <RN.FlatList
@@ -10558,39 +10557,7 @@ flexDirection: 'row',
   keyExtractor={(item, index) => index.toString()}
 />] 
 }}/>
-, (...args:any) => <Elements.ScrollBar pass={{
-            styles: [],
-            arrProps: [],
-            arrItems: [(...args:any) => <Elements.FlatList2 pass={{
-          elementProperties: [
-            `numColumns={3}`
-          ],
-
-          pData: `sc.a3products.images`,
-
-          itemElements: [
-            
-              (...args) => {
-                return (
-                  <RN.Text>
-                    Adicione os campos que quer mostrar aqui.
-                  </RN.Text>
-                );
-              }
-              
-          ],
-
-      styles:[
-              {
-                backgroundColor: 'white',
-                justifyContent: 'center',
-                minHeight: 22,
-                width: "100%",
-              }
-              ],    args,
-        }}/>],
-            args,
-        }}/>],
+],
 
             args,
           }}/>
