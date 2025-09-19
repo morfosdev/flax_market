555e7f7b40a6e425fc0bbb32f5bddae93c772571
Renan <72417996+Rehito0@users.noreply.github.com>
2025-09-19 10:57:12 -0300

    Commit generated by Flaxboll.

--- src/index.tsx
+++ src/index.tsx
@@ -6561,6 +6561,20 @@ fontFamily: 'Inter',
             args,
           }}/>
         , 
+        
+
+          (...args:any) => <Elements.DynView pass={{
+            elementsProperties:['{}'],
+
+            styles:[`{
+height: '30px',
+}`],
+
+            functions:[()=>{}],            childrenItems:[() =><></>],
+
+            args,
+          }}/>
+        , 
 
           (...args:any) => <Elements.DynView pass={{
             elementsProperties:['{}'],
