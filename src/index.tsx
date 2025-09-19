553ce2b2dc65e0e6d635d0bcd5192c53f0e2601d
Leandro <leandrowebmaster@gmail.com>
2025-09-19 13:50:51 -0300

    Commit generated by Flaxboll.

--- src/index.tsx
+++ src/index.tsx
@@ -11519,7 +11519,6 @@ backgroundColor: '#0E1422',
 borderRadius: 4,
 alignItems: 'center',
 justifyContent: 'center',
-opacity: $var_sc.a3.saveButtonDisabled ? 0.5 : 1,
 }`],
 
             functions:[async (...args) =>
