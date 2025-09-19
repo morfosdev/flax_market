026383fe6e4407c26fe93586647c29df873a1ba1
Leandro <leandrowebmaster@gmail.com>
2025-09-19 09:59:07 -0300

    Commit generated by Flaxboll.

--- src/index.tsx
+++ src/index.tsx
@@ -11117,7 +11117,7 @@ alignSelf: 'flex-start',
         
 
           (...args:any) => <Elements.DynView pass={{
-            elementsProperties:[`{ disabled = true, }`],
+            elementsProperties:[`{ disabled: true, }`],
 
             styles:[`{
 width: '138px',
