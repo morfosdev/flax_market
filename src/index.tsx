090cbdcf32aa33e7cc276142e195c4846ad9459d
Renan <72417996+Rehito0@users.noreply.github.com>
2025-12-12 09:30:23 -0300

    Commit generated by Flaxboll.

--- src/index.tsx
+++ src/index.tsx
@@ -28614,7 +28614,7 @@ width: '116px',
 height: '40px',
 }],
 
-      URIvariablePath:[`https://firebasestorage.googleapis.com/v0/b/devs-tests-95208.appspot.com/o/images%2FAdmin.png?alt=media&token=94ebd672-5bdd-4e25-81b7-a1b36e29e6e0`],
+      URIvariablePath:[`https://firebasestorage.googleapis.com/v0/b/devs-tests-95208.appspot.com/o/images%2FmobileFlax.png?alt=media&token=d6f46406-edb3-4a5c-9860-51a3483411c5`],
 
       args,
     }}/>],
