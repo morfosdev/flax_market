a4d862ec646e163d135206c1f00c47ee0d1c1927
Leandro <leandrowebmaster@gmail.com>
2025-12-26 14:32:52 -0300

    Commit generated by Flaxboll.

--- src/index.tsx
+++ src/index.tsx
@@ -15953,7 +15953,7 @@ paddingHorizontal: 15,
               }
               ],
 
-      URIvariablePath:["https://images.unsplash.com/photo-1488831861984-179da3647265?q=80&w=1228&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"],
+      URIvariablePath:[`$var_sc.a4.editData.product.image`],
 
       args,
     }}/>],
