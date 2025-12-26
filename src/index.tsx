4e6df2fdda70203dff9dc8be37cea806e90a35f0
Leandro <leandrowebmaster@gmail.com>
2025-12-26 17:02:13 -0300

    Commit generated by Flaxboll.

--- src/index.tsx
+++ src/index.tsx
@@ -45090,10 +45090,41 @@ alignItems: 'center',
 width: '100%',
 height: '200px',
 flexDirection: 'row',
-backgroundColor: '000',
+backgroundColor: '#000',
 }`],
 
-            functions:[()=>{}],            childrenItems:[() =><></>],
+            functions:[()=>{}],            childrenItems:[
+        (...args:any) => <Elements.Text pass={{
+          arrProps: [
+            '{}'
+          ],
+
+          arrStyles: [
+            { color: 'black', fontSize: 12, }
+          ],
+
+          children: [
+            `Placeholder`
+          ],
+
+          args,
+
+        }}/>, (...args:any) => <Elements.Text pass={{
+          arrProps: [
+            '{}'
+          ],
+
+          arrStyles: [
+            { color: 'black', fontSize: 12, }
+          ],
+
+          children: [
+            `Placeholder`
+          ],
+
+          args,
+
+        }}/>],
 
             args,
           }}/>
