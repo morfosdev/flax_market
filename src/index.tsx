c6ec96422d245f7cb8578e46a4dc7855aaa82707
Leandro <leandrowebmaster@gmail.com>
2025-12-31 13:57:46 -0300

    Commit generated by Flaxboll.

--- src/index.tsx
+++ src/index.tsx
@@ -45413,9 +45413,73 @@ async (...args) =>
             args,
           }}/>
         , (...args:any) => <Elements.ScrollBar pass={{
-            styles: [],
+            styles: [`{
+width: '100%',
+}`],
             arrProps: [],
-            arrItems: [(...args:any) => <Elements.FlatList2 pass={{
+            arrItems: [
+
+          (...args:any) => <Elements.DynView pass={{
+            elementsProperties:['{}'],
+
+            styles:[`{
+flex: 1,
+margin: 10,
+backgroundColor: '#FFF',
+alignItems: 'center',
+paddingHorizontal: 20,
+paddingBottom: 35,
+borderRadius: 10,
+borderWidth: 1,
+borderColor: "#E9E9EB",
+flexDirection: 'row',
+}`],
+
+            functions:[()=>{}],            childrenItems:[
+        
+
+          (...args:any) => <Elements.DynView pass={{
+            elementsProperties:['{}'],
+
+            styles:[
+              {
+                backgroundColor: 'white',
+                justifyContent: 'center',
+                minHeight: 22,
+                width: "100%",
+              }
+              ],
+
+            functions:[()=>{}],            childrenItems:[(...args:any) => <Elements.Text pass={{
+          arrProps: [
+            '{}'
+          ],
+
+          arrStyles: [
+            { color: 'black', fontSize: 12, }
+          ],
+
+          children: [
+            `Placeholder`
+          ],
+
+          args,
+
+        }}/>],
+
+            args,
+          }}/>
+        , 
+
+          (...args:any) => <Elements.DynView pass={{
+            elementsProperties:['{}'],
+
+            styles:[`{
+flex: 1,
+alignItems: 'center',
+}`],
+
+            functions:[()=>{}],            childrenItems:[(...args:any) => <Elements.FlatList2 pass={{
           elementProperties: [
             {}
           ],
@@ -45443,6 +45507,14 @@ async (...args) =>
               }
               ],    args,
         }}/>],
+
+            args,
+          }}/>
+        ],
+
+            args,
+          }}/>
+        ],
             args,
         }}/>],
 
