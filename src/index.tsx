45f0130689d36a24eee710884f768af58d7d0e97
Renan <72417996+Rehito0@users.noreply.github.com>
2026-01-02 11:05:41 -0300

    Commit generated by Flaxboll.

--- src/index.tsx
+++ src/index.tsx
@@ -37840,7 +37840,7 @@ async (...args) =>
         }}), 
         (...args) => {
           // ---------- get Function from A_Project Scope
-          return tools.goTo("b2mobileProducts");
+          return tools.goTo("b5mobileOrders");
         }
         ]
  , trigger: 'on press'
@@ -44148,7 +44148,7 @@ async (...args) =>
         }}), 
         (...args) => {
           // ---------- get Function from A_Project Scope
-          return tools.goTo("b5mobileOrders");
+          return tools.goTo("b2mobileProducts");
         }
         ]
  , trigger: 'on press'
