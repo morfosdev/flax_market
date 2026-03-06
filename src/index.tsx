0361c67399077c9eb4bc6ff83599e8aa87fff26a
Renan <72417996+Rehito0@users.noreply.github.com>
2026-03-06 10:32:58 -0300

    Commit generated by Flaxboll.

--- src/index.tsx
+++ src/index.tsx
@@ -62687,7 +62687,7 @@ justifyContent: 'center',
 'A7': { 'listBox': true } 
 , 'C2': { 'toggles': { 
 'filteredList': false, 
-'filter': false, 'categories': true } 
+'filter': false, 'categories': false } 
  } 
  } 
  
