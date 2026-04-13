8319c94550a7065a502925d5587c60ee3b800e66
Renan <72417996+Rehito0@users.noreply.github.com>
2026-04-13 15:58:52 -0300

    Commit generated by Flaxboll.

--- src/index.tsx
+++ src/index.tsx
@@ -976,7 +976,7 @@ alignSelf: 'baseline',
           ],
 
           children: [
-            `Login`
+            `Recuperação de Senha`
           ],
 
           args,
@@ -1012,7 +1012,7 @@ alignSelf: 'baseline',
           ],
 
           children: [
-            `Acesse sua conta`
+            `Digite seu endereço de e-mail e enviaremos um código seguro para redefinir seu acesso.`
           ],
 
           args,
@@ -1058,7 +1058,7 @@ width: '100%',
           ],
 
           children: [
-            `E-MAIL`
+            `ENDEREÇO DE E-MAIL`
           ],
 
           args,
@@ -1124,197 +1124,6 @@ height: '15px',
             args,
           }}/>
         , 
-        
-
-          (...args:any) => <Elements.DynView pass={{
-            elementsProperties:['{}'],
-
-            styles:[`{
-width: '100%',
-}`],
-
-            functions:[()=>{}],            childrenItems:[
-        
-
-          (...args:any) => <Elements.DynView pass={{
-            elementsProperties:['{}'],
-
-            styles:[`{
-flexDirection: 'row',
-justifyContent: 'space-between',
-alignItems: 'center',
-}`],
-
-            functions:[()=>{}],            childrenItems:[
-        (...args:any) => <Elements.Text pass={{
-          arrProps: [
-            '{}'
-          ],
-
-          arrStyles: [
-            `{ 
-  fontSize: 14, 
-  fontWeight: "500", 
-  fontFamily: "Inter", 
-  color: "#566162",
-  paddingVertical: '5',
-}`
-          ],
-
-          children: [
-            `SENHA`
-          ],
-
-          args,
-
-        }}/>, 
-
-          (...args:any) => <Elements.DynView pass={{
-            elementsProperties:['{}'],
-
-            styles:[`{}`],
-
-            functions:[async (...args) =>
- functions.funcGroup({ args, pass:{
- arrFunctions: [
-        (...args) => {
-          // ---------- get Function from A_Project Scope
-          return tools.goTo("C2recEmail");
-        }
-        ]
- , trigger: 'on press'
-}})],            childrenItems:[(...args:any) => <Elements.Text pass={{
-          arrProps: [
-            '{}'
-          ],
-
-          arrStyles: [
-            `{
-color: '#2D3249',
-fontSize: 12,
-fontFamily: 'Inter',
-fontWeight: '500',
-}`
-          ],
-
-          children: [
-            `Esqueceu sua senha?`
-          ],
-
-          args,
-
-        }}/>],
-
-            args,
-          }}/>
-        ],
-
-            args,
-          }}/>
-        , 
-
-          (...args:any) => <Elements.DynView pass={{
-            elementsProperties:['{}'],
-
-            styles:[`{
-flexDirection: 'row',
-width: '106%',
-}`],
-
-            functions:[()=>{}],            childrenItems:[
-        (...args:any) => <Elements.IptTxtEdit pass={{
-          propsArray: [`{
-	placeholder: 'Digite sua senha',
-	secureTextEntry: "$var_sc.A0.toggles.password"
-}`],
-
-          stylesArray: [`{
-borderRadius: 6,
-borderWidth: 1,
-borderColor: '#E6E7E8',
-padding: 10,
-width: '100%',
-}`],
-
-          path: [`sc.a0.login.forms.password`],
-
-          funcsArray: [async (...args) =>
-        functions.setVar({ args, pass:{
-          keyPath: [`sc.a0.login.forms.password`],
-          value: [`$arg_callback`]
-        }})],
-
-          args,
-        }}/>, 
-
-          (...args:any) => <Elements.DynView pass={{
-            elementsProperties:['{}'],
-
-            styles:[`{
-	width: '20px',
-	height: '20px',
-	alignItems: 'center',
-        right: '35px',
-        top: '13px',
-
-}`],
-
-            functions:[async (...args) =>
- functions.funcGroup({ args, pass:{
- arrFunctions: [() => {
-  const path = "sc.A0.toggles.password";
-  const currState = tools.getCtData(path);
-  tools.functions.setVar({
-		args: '',
-		pass: { keyPath: [path], value: [!currState] },
-	});
-}]
- , trigger: 'on press'
-}})],            childrenItems:[(...args:any) => <Elements.SvgView1 pass={{
-      componentSvg: (Svg:any, SvgObj:any) => {
-        const { Defs, Stop, Path, LinearGradient, G, Circle, Rect, Mask } = SvgObj;
-        return (props:any) => (<Svg
-    xmlns="http://www.w3.org/2000/svg"
-    width={24}
-    height={24}
-    viewBox="0 0 24 24"
-    {...props}
-  >
-    <Path
-      fillRule="evenodd"
-      d="M12 6c-3.233 0-6.04 2.31-7.795 5.796l-.068.136a1.836 1.836 0 0 0-.032.068l.032.068.068.136C5.96 15.69 8.767 18 12 18c3.233 0 6.04-2.31 7.795-5.796l.068-.136.032-.068a1.847 1.847 0 0 0-.032-.068l-.068-.136C18.04 8.31 15.233 6 12 6Zm-9.582 4.896C4.358 7.044 7.72 4 12 4c4.28 0 7.642 3.044 9.581 6.896l.022.043c.091.18.21.412.262.7a2.1 2.1 0 0 1 0 .723 2.525 2.525 0 0 1-.284.742C19.641 16.956 16.281 20 12 20c-4.28 0-7.642-3.044-9.582-6.896l-.021-.043c-.091-.18-.21-.412-.262-.7a2.098 2.098 0 0 1 0-.723c.053-.287.17-.519.262-.7l.021-.042ZM12 10a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm-4 2a4 4 0 1 1 8 0 4 4 0 0 1-8 0Z"
-      clipRule="evenodd"
-    />
-  </Svg>)
-      },
-
-      svgOriginal: `
-        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-<path fill-rule="evenodd" clip-rule="evenodd" d="M12 6C8.76722 6 5.95965 8.31059 4.2048 11.7955C4.17609 11.8526 4.15483 11.8948 4.1369 11.9316C4.12109 11.964 4.11128 11.9853 4.10486 12C4.11128 12.0147 4.12109 12.036 4.1369 12.0684C4.15483 12.1052 4.17609 12.1474 4.2048 12.2045C5.95965 15.6894 8.76722 18 12 18C15.2328 18 18.0404 15.6894 19.7952 12.2045C19.8239 12.1474 19.8452 12.1052 19.8631 12.0684C19.8789 12.036 19.8888 12.0147 19.8952 12C19.8888 11.9853 19.8789 11.964 19.8631 11.9316C19.8452 11.8948 19.8239 11.8526 19.7952 11.7955C18.0404 8.31059 15.2328 6 12 6ZM2.41849 10.896C4.35818 7.04403 7.7198 4 12 4C16.2802 4 19.6419 7.04403 21.5815 10.896C21.5886 10.91 21.5958 10.9242 21.6032 10.9389C21.6945 11.119 21.8124 11.3515 21.8652 11.6381C21.9071 11.8661 21.9071 12.1339 21.8652 12.3619C21.8124 12.6485 21.6945 12.8811 21.6032 13.0611C21.5958 13.0758 21.5886 13.09 21.5815 13.104C19.6419 16.956 16.2802 20 12 20C7.7198 20 4.35818 16.956 2.41849 13.104C2.41148 13.09 2.40424 13.0758 2.39682 13.0611C2.3055 12.881 2.18759 12.6485 2.13485 12.3619C2.09291 12.1339 2.09291 11.8661 2.13485 11.6381C2.18759 11.3515 2.3055 11.119 2.39682 10.9389C2.40424 10.9242 2.41148 10.91 2.41849 10.896ZM12 10C10.8954 10 10 10.8954 10 12C10 13.1046 10.8954 14 12 14C13.1046 14 14 13.1046 14 12C14 10.8954 13.1046 10 12 10ZM8.00002 12C8.00002 9.79086 9.79088 8 12 8C14.2092 8 16 9.79086 16 12C16 14.2091 14.2092 16 12 16C9.79088 16 8.00002 14.2091 8.00002 12Z" fill="#0F1729"/>
-</svg>
-
-      `,
-
-      altura: "15px",
-
-      largura: "15px",
-
-      preenchimento: [`#ccc`],
-
-      args,
-    }}/>],
-
-            args,
-          }}/>
-        ],
-
-            args,
-          }}/>
-        ],
-
-            args,
-          }}/>
-        , 
         (...args:any) => <Elements.Text pass={{
           arrProps: [
             '{}'
@@ -1350,6 +1159,7 @@ height: '30px',
             args,
           }}/>
         , 
+        
 
           (...args:any) => <Elements.DynView pass={{
             elementsProperties:['{}'],
@@ -1421,7 +1231,53 @@ fontFamily: 'Inter',
           ],
 
           children: [
-            `LOGIN`
+            `Enviar código ➔`
+          ],
+
+          args,
+
+        }}/>],
+
+            args,
+          }}/>
+        , 
+
+          (...args:any) => <Elements.DynView pass={{
+            elementsProperties:['{}'],
+
+            styles:[`{
+width: '100%',
+padding: 12,
+backgroundColor: '#2D3249',
+borderRadius: 4,
+alignItems: 'center',
+}`],
+
+            functions:[async (...args) =>
+ functions.funcGroup({ args, pass:{
+ arrFunctions: [
+        (...args) => {
+          // ---------- get Function from A_Project Scope
+          return tools.goTo("ESLogin");
+        }
+        ]
+ , trigger: 'on press'
+}})],            childrenItems:[(...args:any) => <Elements.Text pass={{
+          arrProps: [
+            '{}'
+          ],
+
+          arrStyles: [
+            `{
+color: '#FFF',
+fontWeight: 500,
+fontSize: 14,
+fontFamily: 'Inter',
+}`
+          ],
+
+          children: [
+            `← Voltar para o Login`
           ],
 
           args,
